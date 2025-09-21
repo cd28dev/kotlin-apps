18e64d296b12cd3dd4e8a4b336bbc0b604c23620
Carlos D. Adrianzen Adanaque <carlos.adrianzen27972@gmail.com>
2025-09-21 17:23:55 -0500

    fix: actualizar .gitignore, ajustar AGP en libs.versions.toml y cambios en TiempoUtils

--- app/src/main/java/com/example/listaimagenes/presentation/utils/TiempoUtils.kt
+++ app/src/main/java/com/example/listaimagenes/presentation/utils/TiempoUtils.kt
@@ -1,19 +1,6 @@
 package com.example.listaimagenes.presentation.utils
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import com.example.listaimagenes.presentation.theme.AppTypography
-import com.example.listaimagenes.presentation.theme.ColoresApp
-import com.example.listaimagenes.presentation.theme.Tamaños
+
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
